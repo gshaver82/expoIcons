9c0010d9dbca9bb34d97889131d53ebc84073a82
gshaver <geneshaver@gmail.com>
2023-06-16 14:07:09 -0500

    icon working on preview

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <AntDesign name="exclamationcircle" size={24} color="black" />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
